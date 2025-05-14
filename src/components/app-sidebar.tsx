94365a0a385c422b67e71e2e646a720fed53d2ac
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-05-14 11:24:15 +0700

    feat: update sidebar collapsible with icon

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -78,7 +78,7 @@ const data = {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="offcanvas" {...props}>
+    <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
